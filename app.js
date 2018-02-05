7e679a1cdec8f33585b3168bb11da6c74b08e939
Javier Marquinez <emarquinez@gmail.com>
2018-02-05 21:07:12 +0100

    feat: Rename files, conect auth views

--- app.js
+++ app.js
@@ -4,25 +4,63 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-const index = require('./routes/index');
-
-const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const passport = require('passport');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+
+require('./configs/db.config');
+require('./configs/passport.config').setup(passport);
+
+const index = require('./routes/index.routes');
+const auth = require('./routes/auth.routes');
+const app = express();
+
 mongoose.connect('mongodb://localhost/product-example-development', {});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main-layout');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET || 'SuperSecret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.COOKIE_SECURE || false,
+      httpOnly: true
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60
+    })
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.title = 'Eventure';
+  res.locals.session = req.user || {};
+  res.locals.flash = req.flash() || {};
+  next();
+});
 
 app.use('/', index);
+app.use('/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
